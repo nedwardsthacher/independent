9e89ef752a3710686274414c97cc69b9446e52dd
nedwardsthacher <nedwards@thacher.org>
2017-03-21 17:30:45 -0700

    init commit

---
+++ tabby.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 15:46:50 2017
+
+@author: nickedwards
+"""
+
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+import robust as rb
+import thacherphot as tp
+import hcongrid as h
+from photutils.background import Background2D
+from astropy import wcs
+
